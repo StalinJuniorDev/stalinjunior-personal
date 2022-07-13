e00605e73bc1dd67160b9530d7294f7237112e12
Dılbaz Stalin Junior <80057401+StalinJuniorDev@users.noreply.github.com>
2022-07-13 19:52:15 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const app = express()
+app.use(express.static(process.cwd() + "/public"))
 
 app.get("/", (req, res) => {
-    res.render(process.cwd() + "/views/index.ejs")
+    res.render(process.cwd() + "/public/views/index.ejs")
 })
 
 app.listen(process.env.PORT || 5000, () => {
